0d9aee9ca2890a01ad4d8660fb3852fad08f0a6a
Carl Tibule <cjtibule@outlook.com>
2018-11-06 23:29:44 -0600

    Added proper string representation for Author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,8 @@
 class Author < ApplicationRecord
     has_one_attached :image
+    
+    # Get formatted version of author's name
+    def name
+        return "#{last_name}, #{first_name} #{middle_name}"
+    end
 end
