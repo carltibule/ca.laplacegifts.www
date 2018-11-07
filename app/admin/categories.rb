d6ef47f7dce6803ca01673db91d8e1aa8b364ea1
Carl Tibule <cjtibule@outlook.com>
2018-11-07 16:58:18 -0600

    Reflected one-to-many relationship between Category and Products. Added a row within the ActiveAdmin view of Category to show related products.

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -11,5 +11,19 @@ ActiveAdmin.register Category do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-permit_params :name
+    permit_params :name
+
+    # SHOW view
+    show do
+        attributes_table do
+            row :name
+
+            row 'Products' do |category|
+                category.products.map{ |product|
+                    link_to product.title, admin_product_path(product)
+                }.join("<br/>").html_safe
+            end
+        end
+    end
+
 end
