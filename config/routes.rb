494b9e11d6bb448d488323aeeb43d2c2332db8a7
Carl Tibule <cjtibule@outlook.com>
2018-11-07 18:31:34 -0600

    Added business name, footer and links to 2 existing static links: Home and About Us

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   # Static pages
+  root to: 'pages#index'
   get 'about', to:'pages#about', as: 'about'
 end
