eb50201d7adfcbc743f7392cb8f7e14d1474b03b
Carl Tibule <cjtibule@outlook.com>
2018-11-07 20:38:07 -0600

    Added side navigation bar for categories, and carousel for featured product

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
     def index
+        @categories = Category.all
         @last_ten_products = Product.last(10).reverse
         @first_three_from_last_ten = @last_ten_products.first(3)
         @remaining_seven_from_last_ten = @last_ten_products.last(7)
