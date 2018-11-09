64dd608431d70d0ad522bcb9cac036ab640e29d2
Carl Tibule <cjtibule@outlook.com>
2018-11-08 22:09:59 -0600

    Edited index view of Pages, and generated controller for Category and index and show views for it.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
     def index
+        @categories = Category.all
+        @products_on_sale = Product.where('sale_discount NOT NULL')
     end
 
     def about
