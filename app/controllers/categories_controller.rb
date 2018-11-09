3943ca15f830e772e40a6afbd428e927d835bc24
Carl Tibule <cjtibule@outlook.com>
2018-11-09 12:31:48 -0600

    Added 3 new categories: On sale, New and Updated. Note that these three categories are meant to be empty. On the pages controller, data assigned to these three categories are being pulled from the Products table instead to meet the criteria that its name implies.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,15 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @products_in_tag = @category.products
+    
+    if @category.name == 'On sale'
+      @products_in_tag = Product.where('sale_discount IS NOT NULL')
+    elsif @category.name == 'New'
+      @products_in_tag = Product.order('created_at DESC').limit(10)
+    elsif @category.name == 'Updated'
+      @products_in_tag = Product.order('updated_at DESC').limit(5)
+    else
+      @products_in_tag = @category.products
+    end
   end
 end
