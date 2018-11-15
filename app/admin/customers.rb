c1ba5bf32e2ce21697bd32421fc2e7ff13863814
Carl Tibule <cjtibule@outlook.com>
2018-11-14 19:31:07 -0600

    Customized ActiveAdmin form for Customer. Can't figure out how to implement a single form for home and shipping addresses.

---
+++ app/admin/customers.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register Customer do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+permit_params :first_name, :middle_name, :last_name, :home_address, :shipping_address, :image, :phone_number, :cell_number, :email_address, :password
+
+form do |f|
+    f.inputs do
+      f.input :first_name
+      f.input :middle_name
+      f.input :last_name
+      f.input :image
+      f.input :phone_number
+      f.input :cell_number
+      f.input :email_address
+      f.input :password
+      f.semantic_fields_for :home_address
+    end
+
+    f.actions
+  end
+end
