ea76ef4d30888869cb917d888b248a4f7b1be17d
Carl Tibule <cjtibule@outlook.com>
2018-11-19 22:56:44 -0600

    Added whitelisting on application_controller, added relationship betweeen customer and province in through customer model and fixed sign up form to add select tag for province

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        # devise_parameter_sanitizer.for(:sign_up) << :name
+        # devise_parameter_sanitizer.for(:account_update) << :name
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo,
+        :first_address_line, :second_address_line, :city, :province, :postal_code, :phone_number])
+
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :photo,
+            :first_address_line, :second_address_line, :city, :province, :postal_code, :phone_number])
+    end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,4 @@ class PagesController < ApplicationController
         @categories = Category.all
         @products_on_sale = Product.where('sale_discount NOT NULL')
     end
-
-    def about
-    end
-
-    def register
-        @customer = Customer.new
-    end
 end
